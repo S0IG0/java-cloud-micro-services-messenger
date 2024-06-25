14e6268c94fcd90ae93ad43e1d2f571aa2616ec4
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-06-25 23:53:36 +0300

    feat(MJC-28): add enable discovery client

--- backend/java-cloud/redis-service/src/main/java/ru/soigo/redis/service/RedisServiceApplication.java
+++ backend/java-cloud/redis-service/src/main/java/ru/soigo/redis/service/RedisServiceApplication.java
@@ -2,8 +2,10 @@ package ru.soigo.redis.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RedisServiceApplication {
 
     public static void main(String[] args) {
