269cd5a010ca2dcaf167ba7eedfd93cdf7c494cd
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-06-25 23:50:46 +0300

    feat(MJC-28): add redis service module

---
+++ backend/java-cloud/redis-service/src/main/java/ru/soigo/redis/service/RedisServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.soigo.redis.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisServiceApplication.class, args);
+    }
+
+}
