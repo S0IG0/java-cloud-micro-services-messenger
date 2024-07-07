f0ec9651f0f743cd847cd2aa01920c16c74cd104
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-07-08 00:38:05 +0300

    feat(MJC-26): add connect to redis

---
+++ backend/java-cloud/auth/src/main/java/ru/soigo/auth/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package ru.soigo.auth.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, List<String>> redisTemplate() {
+        RedisTemplate<String, List<String>> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(List.class));
+        return template;
+    }
+
+    @Bean
+    public ListOperations<String, String> listOperations(@NotNull RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+}
