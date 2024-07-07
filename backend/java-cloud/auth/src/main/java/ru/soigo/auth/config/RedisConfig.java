0e9ce5fb4cb330a3459836413061d95d0c086b52
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-07-08 00:41:55 +0300

    feat(MJC-26): add javadoc for redis configuration

--- backend/java-cloud/auth/src/main/java/ru/soigo/auth/config/RedisConfig.java
+++ backend/java-cloud/auth/src/main/java/ru/soigo/auth/config/RedisConfig.java
@@ -12,14 +12,31 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.List;
 
+/**
+ * Configuration class for setting up Redis with Spring.
+ * This configuration class sets up the necessary beans for working with Redis,
+ * including the connection factory, Redis template, and list operations.
+ */
 @Configuration
 public class RedisConfig {
 
+    /**
+     * Creates a {@link RedisConnectionFactory} using Lettuce.
+     *
+     * @return a {@link LettuceConnectionFactory} instance.
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory();
     }
 
+    /**
+     * Configures a {@link RedisTemplate} for serializing and deserializing Redis keys and values.
+     * The keys are serialized using {@link StringRedisSerializer}, and the values are serialized
+     * using {@link GenericToStringSerializer} with {@link List} type.
+     *
+     * @return a configured {@link RedisTemplate} instance.
+     */
     @Bean
     public RedisTemplate<String, List<String>> redisTemplate() {
         RedisTemplate<String, List<String>> template = new RedisTemplate<>();
@@ -29,9 +46,14 @@ public class RedisConfig {
         return template;
     }
 
+    /**
+     * Creates a {@link ListOperations} bean to enable Redis list operations.
+     *
+     * @param redisTemplate the {@link RedisTemplate} to use for list operations.
+     * @return a {@link ListOperations} instance.
+     */
     @Bean
     public ListOperations<String, String> listOperations(@NotNull RedisTemplate<String, String> redisTemplate) {
         return redisTemplate.opsForList();
     }
-
 }
